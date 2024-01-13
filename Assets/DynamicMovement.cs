7ace9afdda9fb602b2908a0ef845d963f8bbe8f4
W4ldschr31n <nicolasrich@hotmail.fr>
2024-01-13 17:49:19 +0100

    [ADD] acceleration

--- Assets/DynamicMovement.cs
+++ Assets/DynamicMovement.cs
@@ -6,7 +6,9 @@ public class DynamicMovement : MonoBehaviour
 {
     // Parameters for movement
     [SerializeField, Range(0f, 10f)]
-    private float speed = 4f;
+    private float maxAcceleration = 10f;
+    [SerializeField, Range(0f, 10f)]
+    private float maxSpeed = 10f;
     [SerializeField, Range(0f, 10f)]
     private float jumpHeight = 2f;
     [SerializeField, Range(0f, 1f)]
@@ -22,6 +24,7 @@ public class DynamicMovement : MonoBehaviour
     // Data indicating how to move
     private float wallJumpX;
     private Vector2 velocity;
+    private float desiredVelocity;
 
     // Start is called before the first frame update
     void Start()
@@ -36,15 +39,36 @@ public class DynamicMovement : MonoBehaviour
         velocity = rgbd.velocity;
 
         // Move left or right
+        float direction = Input.GetKey(KeyCode.LeftArrow) ? -1f : (Input.GetKey(KeyCode.RightArrow) ? 1f : 0f);
+        float desiredSpeed;
+        float desiredAcceleration;
         if (isOnGround)
         {
-            float playerInput = Input.GetAxis("Horizontal");
-            velocity.x = Mathf.Clamp(velocity.x + playerInput * speed, -speed, speed);
+            desiredSpeed = maxSpeed;
         }
-        else if (!isOnWall)
+        else if (isOnWall)
         {
-            float playerInput = Input.GetAxis("Horizontal");
-            velocity.x = Mathf.Clamp(velocity.x + playerInput * speed * airControl, -speed*airControl, speed*airControl);
+            if (direction == -wallJumpX)
+            {
+                desiredSpeed = 0f;
+                velocity.y = -1f * Time.deltaTime;
+            }
+            else
+            {
+                desiredSpeed = maxSpeed * airControl;
+            }
+        }
+        else
+        {
+            desiredSpeed = maxSpeed * airControl;
+        }
+        desiredVelocity = direction * desiredSpeed;
+        if (
+            desiredVelocity <= 0f && velocity.x > desiredVelocity 
+            ||
+            desiredVelocity >= 0f && velocity.x < desiredVelocity)
+        {
+            velocity.x = Mathf.MoveTowards(velocity.x, desiredVelocity, maxAcceleration*Time.deltaTime);
         }
 
         // Jump
@@ -56,6 +80,7 @@ public class DynamicMovement : MonoBehaviour
         // Update velocity
         Debug.Log(velocity.x);
         rgbd.velocity = velocity;
+        
     }
 
     private void Jump()
@@ -69,7 +94,7 @@ public class DynamicMovement : MonoBehaviour
         // Wall jump
         else if (isOnWall)
         {
-            velocity.x = wallJumpX * 10f;
+            velocity.x = wallJumpX * 20f;
             velocity.y = Mathf.Sqrt(-2f * Physics2D.gravity.y * jumpHeight);
             Debug.Log("Wall Jump");
         }
@@ -84,16 +109,6 @@ public class DynamicMovement : MonoBehaviour
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
-    {
-        EvaluateCollision(collision);
-    }
-
-    private void OnCollisionStay2D(Collision2D collision)
-    {
-        EvaluateCollision(collision);
-    }
-
-    private void EvaluateCollision(Collision2D collision)
     {
         int col_layer = collision.gameObject.layer;
         // Player is on the ground
@@ -117,6 +132,43 @@ public class DynamicMovement : MonoBehaviour
                 {
                     isOnWall |= true;
                     velocity.x = 0f;
+                    wallJumpX = collision.GetContact(i).normal.x;
+                }
+            }
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        int col_layer = collision.gameObject.layer;
+        // Player is on the ground
+        if (col_layer == LayerMask.NameToLayer("Ground"))
+        {
+            for (int i = 0; i < collision.contactCount; i++)
+            {
+                if (collision.GetContact(i).normal.y >= 0.9f)
+                {
+                    isOnGround |= true;
+                    doubleJump |= true;
+                }
+            }
+            if (collision.gameObject.CompareTag("Ice"))
+            {
+                maxAcceleration = maxSpeed / 2;
+            }
+            else
+            {
+                maxAcceleration = maxSpeed;
+            }
+        }
+        // Player is hugging a wall
+        else if (col_layer == LayerMask.NameToLayer("Wall"))
+        {
+            for (int i = 0; i < collision.contactCount; i++)
+            {
+                if (collision.GetContact(i).normal.y <= 0.1f)
+                {
+                    isOnWall |= true;
                     wallJumpX = collision.GetContact(i).normal.x;
                 }
             }

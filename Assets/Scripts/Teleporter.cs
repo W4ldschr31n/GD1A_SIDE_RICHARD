5b4907fb6ac4d34a9603073693ffd6125d0f2ed2
W4ldschr31n <nicolasrich@hotmail.fr>
2024-02-15 15:49:53 +0100

    [ADD] teleporter

---
+++ Assets/Scripts/Teleporter.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour
+{
+    [SerializeField]
+    private Transform exitLocation;
+    [SerializeField]
+    private Rigidbody2D playerBody;
+
+    private bool canInteract = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canInteract && Input.GetButtonDown("Fire1")){
+            Debug.Log("OSKOUR");
+            playerBody.transform.position = exitLocation.position;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        canInteract = true;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        canInteract = false;
+    }
+}

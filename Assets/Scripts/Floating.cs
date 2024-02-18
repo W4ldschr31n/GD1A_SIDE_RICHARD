828b77c6ff757cc0a9e0defaf0bc46f7a4a317f1
W4ldschr31n <nicolasrich@hotmail.fr>
2024-02-18 17:13:01 +0100

    better collectibles

---
+++ Assets/Scripts/Floating.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floating : MonoBehaviour
+{
+
+    [SerializeField]
+    private float amplitude;
+
+    private float originalY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Don't forget where we started
+        originalY = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Ride along the sine wave
+        float newY = originalY + Mathf.Sin(Time.time) * amplitude;
+        transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+    }
+}

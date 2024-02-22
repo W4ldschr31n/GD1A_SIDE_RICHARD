4daee96923e3bc3218eef84acb49d3fd828d11a2
W4ldschr31n <nicolasrich@hotmail.fr>
2024-02-22 22:58:40 +0100

    [ADD] gates and tag sprites

---
+++ Assets/Scripts/Lever.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lever : MonoBehaviour
+{
+    private bool playerIsNearby = false;
+    private bool isActivated = false;
+    [SerializeField]
+    private GameObject gate;
+    [SerializeField]
+    private PlayerBehaviour player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerIsNearby && !isActivated && Input.GetButtonDown("Use"))
+        {
+            Destroy(gate);
+            isActivated = true;
+            GetComponent<SpriteRenderer>().flipY = true;
+            player.ShowOverheadMessage("Un mécanisme se fait entendre. Une porte semble s'être ouverte.", 3);
+            // Doesn't seem to work :(
+            GetComponent<MessageWhenNearby>().enabled = false;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        playerIsNearby = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        playerIsNearby = false;
+    }
+}
